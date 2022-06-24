bb4d242c65c89f7f1e349e81e07728d9f48dd10a
Alichevictor <alichevictor@gmail.com>
2022-06-24 21:01:20 +0100

    yes

---
+++ calculator/sandbox.js
@@ -0,0 +1,24 @@
+// math operators +, -, /, *,
+
+
+let likes = 10;
+
+//likes = likes + 1;
+
+console.log(likes);
+//likes += 10;
+
+console.log(likes);
+
+//likes -= 5;
+
+likes *= 2;
+
+console.log(likes);
+
+likes /= 2;
+
+console.log(likes);
+
+
+
